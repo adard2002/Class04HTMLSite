cd05c8b6895b0482d9430bf178588e72ccf6069d
adard2002 <adard2002@gmail.com>
2020-12-16 15:37:52 -0600

    got the js set up from the class demo

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+// // var greeting;
+
+// // var userName = prompt('Please enter your first name.');
+
+// // console.log(userName);
+
+// //var today = new Date();
+// // console.log(today);
+
+// // var hourNow = prompt('What is the current time of day?, Military time 0-23');
+
+// if(hourNow > 18 && hourNow <24)
+// {
+//     greeting = 'Good Evening.';
+// } 
+// else if (hourNow > 12 && hourNow < 18 ) 
+// {
+//     greeting = 'Happy Afternoon.';
+// } 
+// else if (hourNow < 12 && hourNow > 0 )
+// {
+//     greeting = 'Good Morning';
+// }
+// else
+//  {
+//     greeting = "Something went wrong.. Sorry.";
+//  }
+
+
+
+// document.write('<h1>' + greeting + '</h1>');
