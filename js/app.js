5d1bd9ccc51b8f77e26893f68e76deaf457b0037
adard2002 <adard2002@gmail.com>
2020-12-16 19:12:42 -0600

    tried to fix the JS but I'm lost

--- js/app.js
+++ js/app.js
@@ -9,34 +9,38 @@
 
 
 
-// // var greeting;
+var greeting;
 
-// // var userName = prompt('Please enter your first name.');
+var userName = prompt('Please enter your first name.');
 
-// // console.log(userName);
+console.log(userName);
 
-// //var today = new Date();
-// // console.log(today);
+var today = new Date();
+console.log(today);
 
-// // var hourNow = prompt('What is the current time of day?, Military time 0-23');
-
-// if(hourNow > 18 && hourNow <24)
-// {
-//     greeting = 'Good Evening.';
-// } 
-// else if (hourNow > 12 && hourNow < 18 ) 
-// {
-//     greeting = 'Happy Afternoon.';
-// } 
-// else if (hourNow < 12 && hourNow > 0 )
-// {
-//     greeting = 'Good Morning';
-// }
-// else
-//  {
-//     greeting = "Something went wrong.. Sorry.";
-//  }
+var hourNow = prompt('What is the current time of day?, Military time 0-23');
 
 
 
-// document.write('<h1>' + greeting + '</h1>');
+// I did try to do what Craig did but I might've missed something I'm not sure. I'll try to look through this tomorrow again. Sorry it's getting a bit late and I've been stressing all day over JS. css and html make a little bit more sense though.
+
+if(hourNow > 18 && hourNow <24)
+{
+    greeting = 'Good Evening.';
+} 
+else if (hourNow > 12 && hourNow < 18 ) 
+{
+    greeting = 'Happy Afternoon.';
+} 
+else if (hourNow < 12 && hourNow > 0 )
+{
+    greeting = 'Good Morning';
+}
+else
+ {
+    greeting = "Something went wrong.. Sorry.";
+ }
+
+
+
+document.write('<h1>' + greeting + '</h1>');
