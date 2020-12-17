53266c550a108ae01e497db9089c00463f4856a4
adard2002 <adard2002@gmail.com>
2020-12-17 13:01:51 -0600

    added scrollbar and some js. since it was in my other project im gonna look through it and try to understand everything its saying

--- js/app.js
+++ js/app.js
@@ -1,46 +1,31 @@
-
-
-
-
-
-
-
-
-
-
-
-var greeting;
-
-var userName = prompt('Please enter your first name.');
-
+'use strict';
 console.log(userName);
-
+var userName = prompt('Please enter your first name.');
+alert('Hello ' + userName + ', click ok to go to page.');
 var today = new Date();
 console.log(today);
 
+
+
+
 var hourNow = prompt('What is the current time of day?, Military time 0-23');
+var greeting;
 
+function runGreeting(){
 
-
-// I did try to do what Craig did but I might've missed something I'm not sure. I'll try to look through this tomorrow again. Sorry it's getting a bit late and I've been stressing all day over JS. css and html make a little bit more sense though.
-
-if(hourNow > 18 && hourNow <24)
-{
-    greeting = 'Good Evening.';
-} 
-else if (hourNow > 12 && hourNow < 18 ) 
-{
-    greeting = 'Happy Afternoon.';
-} 
-else if (hourNow < 12 && hourNow > 0 )
+if (hourNow > 18 && hourNow < 24) { 
+    greeting = 'Good evening.' + userName + '!';
+} else if (hourNow > 12 && hourNow < 18 ) {
+    greeting = 'Good afternoon, ' + userName + '!';
+} else if (hourNow >= 0 && hourNow < 12 )
 {
     greeting = 'Good Morning';
-}
-else
- {
-    greeting = "Something went wrong.. Sorry.";
+} else {
+    greeting = "Something went wrong!";
  }
 
+ document.write('<h1 style="color:yellow">' + greeting + '</h1>');
 
+} //this closes the function
 
-document.write('<h1>' + greeting + '</h1>');
+runGreeting();
